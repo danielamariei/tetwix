6b933714a3cb12e0627074e665b99f72b9996f42
Amariei G. Daniel <daniel.g.amariei@gmail.com>
2014-06-08 14:43:35 +0300

    Organized the code.

---
+++ public/javascripts/debugging.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: workstation
+ * Date: 6/8/14
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+/* Class that helps debugging */
+var Debugging = function () {
+    this.output = document.getElementById('debugging');
+    this.LOG = function (text) {
+        this.output.innerHTML += text;
+    };
+
+    this.LOG_LINE = function (text) {
+        this.LOG(text + "<br />");
+    };
+};
+
+
+var Debug = new Debugging();
