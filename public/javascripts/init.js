ef81bcf689eff90051b3efcee3250e747df8be09
Iordan Rata <iordan.rata@gmail.com>
2014-06-11 11:47:04 +0300

    Fixed navigate to home screen bug. Added buttons hover description

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,4 @@
+var isGameInit = false;
 /**-----------------*/
 $('#game-over').hide();
 $('#game-wrapper').hide();
@@ -8,10 +9,19 @@ $('#share-score').hover(function() {
 });
 
 /**-----------------*/
-$("#start-screen" ).delegate("#campaign", "click", function() {
-    $("#game-wrapper").show();
-    $("#start-screen").remove();
-    game.controls.startNewGame(); });
+function startNewGame() {
+    if (!isGameInit) {
+        $("#game-wrapper").show();
+        $("#start-screen").hide();
+        game.controls.startNewGame();
+        isGameInit = true;
+    } else {
+        $("#game-wrapper").show();
+        $("#start-screen").hide();
+        game.controls.resume()
+    }
+}
+$("#start-screen" ).delegate("#campaign", "click", startNewGame);
 
 /**-----------------*/
 $("#game-wrapper" ).delegate("#share-score", "click", function() {
@@ -24,20 +34,25 @@ $("#game-wrapper" ).delegate("#restart", "click", function() {
 
 /**-----------------*/
 $("#game-wrapper" ).delegate("#start", "click", function() {
-    $("#game-wrapper").remove();
+
+    $("#game-wrapper").hide();
     $('#start-screen').show();
+    game.controls.restart()
+    game.controls.pause();
 });
 
 $("#game-wrapper" ).delegate("#control.play", "click", function() {
-    game.controls.resume();
     $("#control").removeClass("play");
     $("#control").addClass("pause");
+    $("#control").attr('title', "Pause")
+    game.controls.resume();
 });
 
 $("#game-wrapper" ).delegate("#control.pause", "click", function() {
-    game.controls.pause();
     $("#control").removeClass("pause");
     $("#control").addClass("play");
+    $("#control").attr('title', "Play")
+    game.controls.pause();
 });
 
 $("#game-wrapper" ).delegate("#settings", "click", function() {
